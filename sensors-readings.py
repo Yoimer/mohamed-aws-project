bc59556626d258d297d1c07b934829c276b248f1
Yoimer Román <yoimer.roman@technoglobalinc.com>
2020-04-07 20:53:47 -0400

    modified sensors-readings.py saving date time voltage as load_voltage.csv

--- sensors-readings.py
+++ sensors-readings.py
@@ -1,7 +1,6 @@
 """Sample code and test for adafruit_in219 and SW-420 accelerometer sensors"""
 
 # adafruit_in219 libraries
-import time
 import board
 from adafruit_ina219 import ADCResolution, BusVoltageRange, INA219
 
@@ -20,7 +19,8 @@ import os.path
 
 # time libraries
 from datetime import datetime
-
+import datetime
+import time
 
 # display some of the advanced field (just to test)
 print("Config register:")
@@ -63,82 +63,148 @@ while True:
     time.sleep(2)
     print("")
 
-    
+    # date,temperature
+    # 2019-11-14 00:00:00.000,51.5
+    # 2019-11-14 01:00:00.000,56.2
+    # 2019-11-14 02:00:00.000,58.3
+    # 2019-11-14 03:00:00.000,62.4
+    # 2019-11-14 04:00:00.000,66.5
+    # 2019-11-14 05:00:00.000,70.6
+    # 2019-11-14 06:00:00.000,65.7
+    # 2019-11-14 07:00:00.000,45.8
+
+    # datetime.datetime.utcnow()
+    # 2020-04-07 23:39:51.679200
+
     # datetime object containing current date and time
-    now = datetime.now()
+    # now = datetime.now()
 
     # saves sensor data in list
     list_to_file = []
 
+    # creates specific header depending on the variable
+    headers_list = []
+    headers_list.append("Load-Voltage(V)")                 # 0
+    headers_list.append("Current(A)")                      # 1
+    headers_list.append("Power(W)")                        # 2
+    headers_list.append("Coordinate-X")                    # 3
+    headers_list.append("Coordinate-Y")                    # 4
+    headers_list.append("Coordinate-Z")                    # 5
+
     # date and time of reading
 
-    # day/month/year
-    temp = now.strftime("%d/%m/%Y") + ","
+    # date space and time utc format
+    # 2020-04-07 23:39:51.679200
+    temp = str(datetime.datetime.utcnow()) + ","
     list_to_file.append(temp)
 
-    # hour/minute/second
-    temp = now.strftime("%H:%M:%S") + ","
-    list_to_file.append(temp)
-
-    # bus_voltage + shunt_voltage
-    # temp = "{:6.3f}".format(bus_voltage + shunt_voltage) + ","
-    # list_to_file.append(temp)
-
-    # shunt_voltage
-    # temp = "{:9.6f}".format(shunt_voltage) + ","
-    # list_to_file.append(temp)
-
-    # bus_voltage
-    # temp = "{:6.3f}".format(bus_voltage) + ","
-    # list_to_file.append(temp)
-
+    ######################################## load voltage
     # load voltage
-    temp = "{:6.3f}".format(12.0) + ","
+    temp = "{:6.3f}".format(12.0) + "\r\n"
     list_to_file.append(temp)
 
-    # current
-    temp = "{:9.6f}".format(current / 1000) + ","
-    list_to_file.append(temp)
-
-    # power
-    temp = "{:6.3f}".format((current / 1000) * 12) + ","
-    list_to_file.append(temp)
-
-    # coordinates
-    xyz = ("%f %f %f"%accelerometer.acceleration).split()
-
-    # coordinate x
-    temp = (xyz[0]) + ","
-    list_to_file.append(temp)
-
-    # coordinate y
-    temp = (xyz[1]) + ","
-    list_to_file.append(temp)
-
-    # coordinate z
-    temp = (xyz[2]) + "\r\n"
-    list_to_file.append(temp)
-
-    # headers = "Date,Time,PSU-Voltage(V),Shunt-Voltage(V),Bus-Voltage(V),Current(A),Power(W),Coordinate-X,Coordinate-Y,Coordinate-Z" + "\r\n"
-    headers = "Date,Time,Load-Voltage(V),Current(A),Power(W),Coordinate-X,Coordinate-Y,Coordinate-Z" + "\r\n"
-
+    headers = "Date," + headers_list[0] + "\r\n"
     # checks if file already exists
-    if(os.path.isfile('/home/pi/sensors.csv')):
+    if(os.path.isfile('/home/pi/project/load_voltage.csv')):
+        print("File exists")
+        time.sleep(1)
         # adds only new values to file
-        # print("file exists already")
-        f = open("sensors.csv", "a+")
+        f = open("load_voltage.csv", "a+")
         for i in range(0, len(list_to_file)):
             print(list_to_file[i])
             f.write(list_to_file[i])
         f.close()
     else:
+        print("File does not exist, creating")
+        time.sleep(1)
         # creates file including headers
-        # print("file does not exist")
-        f = open("sensors.csv", "a+")
+        f = open("load_voltage.csv", "a+")
         f.write(headers)
         for i in range(0, len(list_to_file)):
             print(list_to_file[i])
             f.write(list_to_file[i])
         f.close()
+
+    ######################################## current
+    # current
+    # temp = "{:9.6f}".format(current / 1000) + ","
+    # list_to_file.append(temp)
+
+    # headers = "Date," + headers_list[1] + "\r\n"
+    # # checks if file already exists
+    # if(os.path.isfile('/home/pi/current.csv')):
+    #     # adds only new values to file
+    #     f = open("current.csv", "a+")
+    #     for i in range(0, len(list_to_file)):
+    #         print(list_to_file[i])
+    #         f.write(list_to_file[i])
+    #     f.close()
+    #     list_to_file.clear()
+    # else:
+    #     # creates file including headers
+    #     f = open("current.csv", "a+")
+    #     f.write(headers)
+    #     for i in range(0, len(list_to_file)):
+    #         print(list_to_file[i])
+    #         f.write(list_to_file[i])
+    #     f.close()
+    #     list_to_file.clear()
+
+    # # day/month/year
+    # temp = now.strftime("%d/%m/%Y") + ","
+    # list_to_file.append(temp)
+
+    # # hour/minute/second
+    # temp = now.strftime("%H:%M:%S") + ","
+    # list_to_file.append(temp)
+
+    # # load voltage
+    # temp = "{:6.3f}".format(12.0) + ","
+    # list_to_file.append(temp)
+
+    # # current
+    # temp = "{:9.6f}".format(current / 1000) + ","
+    # list_to_file.append(temp)
+
+    # # power
+    # temp = "{:6.3f}".format((current / 1000) * 12) + ","
+    # list_to_file.append(temp)
+
+    # # coordinates
+    # xyz = ("%f %f %f"%accelerometer.acceleration).split()
+
+    # # coordinate x
+    # temp = (xyz[0]) + ","
+    # list_to_file.append(temp)
+
+    # # coordinate y
+    # temp = (xyz[1]) + ","
+    # list_to_file.append(temp)
+
+    # # coordinate z
+    # temp = (xyz[2]) + "\r\n"
+    # list_to_file.append(temp)
+
+    # creates sensor.csv including all the values
+    # headers = "Date,Time,Load-Voltage(V),Current(A),Power(W),Coordinate-X,Coordinate-Y,Coordinate-Z" + "\r\n"
+
+    # # checks if file already exists
+    # if(os.path.isfile('/home/pi/sensors.csv')):
+    #     # adds only new values to file
+    #     # print("file exists already")
+    #     f = open("sensors.csv", "a+")
+    #     for i in range(0, len(list_to_file)):
+    #         print(list_to_file[i])
+    #         f.write(list_to_file[i])
+    #     f.close()
+    # else:
+    #     # creates file including headers
+    #     # print("file does not exist")
+    #     f = open("sensors.csv", "a+")
+    #     f.write(headers)
+    #     for i in range(0, len(list_to_file)):
+    #         print(list_to_file[i])
+    #         f.write(list_to_file[i])
+    #     f.close()
 
     time.sleep(2)
